6aff69c3b3604d2564b16c2910c36664a016bbf8
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-07-05 17:59:57 +0530

    Update 1-first-app.js

--- 4-first-app/1-first-app.js
+++ 4-first-app/1-first-app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-	response.write('Hello from different World\n');
+	response.write('Hello  World\n');
 	response.end();
 });
 
